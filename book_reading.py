1f1256bfe3eeb6123b1c9a6c2fb55d43cfd7b2c1
Bramdv <bramdevroey@gmail.com>
2021-04-29 15:54:49 +0200

    changed book parser more general

--- book_reading.py
+++ book_reading.py
@@ -1,15 +1,17 @@
-from bs4 import BeautifulSoup as bs
 import logging
 import re
+
+from bs4 import BeautifulSoup as bs
 
 logger = logging.getLogger(__name__)
 logger.setLevel(-1)
 
 
 class Book:
-    """Parser object for Project Gutenberg books. Easily retrieve chapters, or other parts."""
+    """Parser object for Project Gutenberg books. Easily retrieve chapters, or
+    other parts."""
 
-    def __init__(self, filename):
+    def __init__(self, filename, chapter_tag="h3"):
         """Create a new Book object. """
         # logging
         self.logger = logger.getChild("Book")
@@ -29,42 +31,32 @@ class Book:
         self.book_title = self.soup.find("h1").text.strip()
         self.book_author = self.soup.find("h2").text.strip().lstrip("by ")
 
-        self.chapter_names = [
-            title.text.strip() for title in self.soup.select(".chapter h3")
-        ]
+        # chapter detection
+        self._chapter_tag = chapter_tag
 
-        if len(self.soup.select(".chapter h4")) > 0:
-            self.chapter_tree = [
-                [title.text.strip() for title in chapter.select("h4")]
-                for chapter in self.soup.select(".chapter")
-            ]
+        self.chapter_names = [
+            title.text.strip()
+            for title in self.soup.select(f".chapter {self._chapter_tag}")
+        ]
 
         self.logger.debug(
             f"Book {self.book_title} by {self.book_author}, has {len(self.chapter_names)} chapters."
         )
 
-    def _get_chapter(self, n):
-        """Retuns a soup object for chapter n. (Starting at zero.)"""
-        return self.soup.select(".chapter")[n]
-
-    def get_paragraphs(self, chapter=None, section=None):
-        """Generator for all paragraphs in a selected chapter (and section). If they are not specified, the full book will be traversed."""
+    def get_paragraphs(self, chapter=None):
+        """Generator for all paragraphs in a selected chapter (and section). If
+        they are not specified, the full book will be traversed."""
 
         # select body of the chapter
         if chapter is None:
             current_p = self.soup.body.find("p")
         else:
-            current_p = self._get_chapter(chapter).find("p")
-        # subsection case
-        if section is not None and len(self.chapter_tree[chapter]) > 0:
-            current_p = current_p.parent.select("h4")[section]
+            current_p = self._get_chapter(chapter)
 
         # loop
         while current_p is not None:
-            if section is not None:
-                if current_p.nextSibling.name == "h4":
-                    break
-
+            if current_p.nextSibling.name == self._chapter_tag:
+                break
             if current_p.name == "p":
                 p = re.sub(r"\s+", " ", current_p.get_text(strip=True, separator=" "))
                 # take care of big stylish character at beginning
@@ -89,8 +81,9 @@ if __name__ == "__main__":
     )
     logger.addHandler(logstream)
 
-    logger.info("Loading Sherlock.html as a test.")
-    test_book = Book("books/Sherlock.html")
-    p = test_book.get_paragraphs(0, 0)
-    print(p.__next__())
+    test_file = "books/80days.html"
+    logger.info("Loading {test_file} as a test.")
+    test_book = Book(test_file, chapter_tag="h4")
+    p = test_book.get_paragraphs(0)
+    print([*p])
     pass
