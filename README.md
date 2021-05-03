22717bfb4854b2e27b6c8121e2d0351c08fd8df3
Bramdv <bramdevroey@gmail.com>
2021-05-03 15:50:07 +0200

    bugfix gutenberg parser

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
 # Text summarization with NLP
+
 The goal of this project, on behalf of [Algorhythm](https://algorhythm.be/), is to explore the current state of Natural Language Processing for text summarization.
 
-## Data sources
+## Technical details
+
+### Data sources
+
 The methods are tested on [books](https://www.gutenberg.org/) that are in the public domain. 
 
-## Deliverables 
+### Deliverables
+
 - **Project Gutenberg** [book parser](book_reading.py), to extract paragraphs from selected chapters.
+- 
 
-## Installation and usage
+### Installation and usage
 
+This project is mainly built on HuggingFace's transformers library, as it provides an easy way into the newest pre-trained models. I opted for the 
 
-## Notable conclusions about novel summarization
-Automatic text summarization is hard. Without the full life experience of a human being behind it, all algorithms—even the most state-of-the-art deep learning backed ones—have to rely on methods that fake understanding of the text.
+## Notable conclusions
 
-These methods can produce quite impressive results for news articles and other nonfiction, though. However, summarizing literature must be the most difficult thing to do. Here are some ways stories differ from more straightforward texts.
+**Automatic text summarization is hard.** Without the full life experience of a human being behind it, all algorithms—even the most state-of-the-art deep learning backed ones—have to rely on methods that fake understanding of the text.
 
-  - Stories can **not** be summarized with **extractive methods**, i.e. picking and choosing the right sentences to get the most important ideas. Moreover, sentences often depend on their **context** and don’t mean much without their siblings.
-  - **Long**, **stylistic** sentences, that can hold important information.
-  - **Dialogue** is insanely complicated. Not only does it matter who says what, it is not even easy to pin-point the speaker, as for long dialogues, this is often represented merely by the alternation of quotations.
+These methods can produce quite impressive results for news articles and other nonfiction, though. However, summarizing literature must be the most difficult thing to do. Here are some ways stories differ from more straightforward informative texts.
+
+- Stories can **not** be summarized with **extractive methods**, i.e. picking and choosing the right sentences to get the most important ideas. Moreover, sentences often depend on their **context** and don’t mean much without their siblings.
+- **Long**, **stylistic** sentences, that can hold important information.
+- **Dialogue** is insanely complicated. Not only does it matter who says what, it is not even easy to pin-point the speaker, as for long dialogues, this is often represented merely by the alternation of quotations.
 
 In order to get good results for book summaries, we need to do some smart preprocessing. 
--- book_reading.py
+++ book_reading.py
@@ -1,5 +1,6 @@
 import logging
 import re
+from collections import Counter
 
 from bs4 import BeautifulSoup as bs
 
@@ -11,8 +12,12 @@ class Book:
     """Parser object for Project Gutenberg books. Easily retrieve chapters, or
     other parts."""
 
-    def __init__(self, filename, chapter_tag="h3"):
-        """Create a new Book object. """
+    def __init__(self, filename: str, chapter_tag: str = None):
+        """Create a new Book object.
+
+        :param filename: Path to html file
+        :param chapter_tag: html tag that
+        """
         # logging
         self.logger = logger.getChild("Book")
 
@@ -23,20 +28,40 @@ class Book:
                 self.soup = bs(f.read(), features="html.parser")
         except FileNotFoundError:
             self.logger.critical(f"File {filename} not found.")
+            raise FileNotFoundError()
         except Exception:
             self.logger.exception("Problem with parsing html file.")
+            raise Exception
 
         # get the meta information
         self.logger.info("Parsing meta information.")
         try:
-            self.book_title = self.soup.find("h1").text.strip()
-            self.book_author = self.soup.find("h2").text.strip().lstrip("by ")
+            # look for title and author in first 1000 chars of body source
+            self.book_author = re.search(
+                r"Author: ([^<\n]+)", str(self.soup.body)[:1000]
+            ).group(1)
+            self.book_title = re.search(
+                r"Title: ([^<\n]+)", str(self.soup.body)[:1000]
+            ).group(1)
         except:
+            self.logger.warning(f"Title and auhtor not automatically found")
             self.book_title = self.soup.find("h1").text.strip()
-            self.book_author = "AUTHOR"
+            self.book_author = "AUTHOR NOT FOUND"
 
         # chapter detection
-        self._chapter_tag = chapter_tag
+        if chapter_tag is not None:
+            self._chapter_tag = chapter_tag
+        else:
+            # try to detect chapter tag automatically
+            self.logger.info(
+                "Chapter tag not specified, trying to detect from context."
+            )
+            # count all headers
+            header_count = Counter(
+                [header.name for header in self.soup.find_all(re.compile("^h[1-6]$"))]
+            )
+            self._chapter_tag = header_count.most_common(1)[0][0]
+            self.logger.debug(f"Selected {self._chapter_tag} for chapter headers.")
 
         self.chapter_names = [
             title.text.strip() for title in self.soup.select(f"{self._chapter_tag}")
@@ -46,9 +71,11 @@ class Book:
             f"Book {self.book_title} by {self.book_author}, has {len(self.chapter_names)} chapters."
         )
 
-    def get_paragraphs(self, chapter=None):
-        """Generator for all paragraphs in a selected chapter (and section). If
-        they are not specified, the full book will be traversed."""
+    def get_paragraphs(self, chapter: int = None):
+        """Generator for all paragraphs in a selected chapter (and section).
+
+        :param chapter: chapter number, if not specified, the full book will be traversed.
+        """
 
         # select body of the chapter
         if chapter is None:
@@ -70,6 +97,17 @@ class Book:
 
             current_p = current_p.nextSibling
 
+    def get_chapter(self, chapter: int, merge: bool = False):
+        """Return al text of chapter with given number.
+
+        :param chapter: chapter number
+        :param merge: Whether paragraphs should be merged, separated by two newlines, default False.
+        """
+        if merge:
+            return "\n\n".join([*self.get_paragraphs(chapter)])
+        else:
+            return [*self.get_paragraphs(chapter)]
+
 
 if __name__ == "__main__":
     # setup basic logger
@@ -85,8 +123,8 @@ if __name__ == "__main__":
     logger.addHandler(logstream)
 
     test_file = "books/80days.html"
-    logger.info("Loading {test_file} as a test.")
-    test_book = Book(test_file, chapter_tag="h4")
-    p = test_book.get_paragraphs(0)
-    print([*p])
+    logger.info(f"Loading {test_file} as a test.")
+    test_book = Book(test_file)
+    p = test_book.get_paragraphs(1)
+    # print([*p])
     pass
